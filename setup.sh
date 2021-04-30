43a7d7ba3863bb16db3d11e87e30afcc5ba5565d
Nuno <nunocalaim@gmail.com>
2021-04-30 12:16:27 +0100

    update

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ sleep 5
 sudo apt-get -y update
 sleep 40
 sudo apt -y install python3-pip
-sleep 15
+sleep 40
 cd Setup-Flask-Machine/python_flask
-sleep 5
+sleep 4
 pip3 install -r requirements.txt
-sleep 5
+sleep 20
 pip3 install connexion[swagger-ui]
-sleep 5
+sleep 10
 nohup python3 -m tracking_server &
